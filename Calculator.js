d24e8bcde241a970fd48f9037dd62cdbc795b768
germanAI_work <denisskinrasskaz@ya.ru>
2019-01-14 16:39:54 +0300

    - 2

--- Calculator.js
+++ Calculator.js
@@ -1,15 +1,38 @@
+$(function() {
+
+  var $element = $(".calculator");
+  if(!$element.length) return;
+
+  var config;
+  $.getJSON("config.json", function(data) {
+    config = data;
+    
+    var extraActions = {'current-year': function (){ return new Date().getFullYear();}}
+
+    let calculator = new Calculator( config, $element, extraActions );
+  });
+
+})
+
+
+
+
+
+//
 class Calculator {
 
-  constructor( config_data, calculator, extraActions ) {
+  constructor( config_data, $container, extraActions ) {
+    
+    if( !$container.length ) return;
+
     //
     this.actions_names = {}
     
   	//
   	this.operations_story = '';
-    this.screen = $('.screen');
 
   	//
-  	this.calculator = calculator;
+  	this.$container = $container;
 
   	//
     this.buttons = {};
@@ -17,30 +40,39 @@ class Calculator {
 
     //
   	if( config_data ) this.bindActions( config_data );
-  	if ( calculator ) {
-      this.createCalculatorInterface(config_data, calculator)
-      this.initActionsNames(extraActions);
-  		this.initMouseInput( calculator );
-  		this.initKeyboardInput( calculator );
-  	}
+
+    this.initActionsNames(extraActions);
+
+    this.createCalculatorInterface(config_data, $container);
+    // this.$screen = $('.screen', $container );
+    // this.$screen = $container.find('.screen');    
+
+		// this.initMouseInput( $container );
+		this.initKeyboardInput( $container );
+  	
   }
 
   initActionsNames(extraActions) {
-    this.actions_names['remove-last'] = this.removeLastSymbol.bind(this);
-    this.actions_names['result'] = this.showResult.bind(this);
-    this.actions_names['clear-input'] = this.clearInput.bind(this);
-    this.actions_names['invert-sign'] = this.invertSign.bind(this);
-    this.actions_names['square'] = this.square.bind(this);
-    this.actions_names['reciprocal'] = this.reciprocal.bind(this);
-    this.actions_names['devide'] = this.printOperationSymbol.bind(this);
-    this.actions_names['multiply'] = this.printOperationSymbol.bind(this);
-    this.actions_names['subtraction'] = this.printOperationSymbol.bind(this);
-    this.actions_names['addition'] = this.printOperationSymbol.bind(this);
+
+    this.actions_names = {
+      'remove-last' : this.removeLastSymbol.bind(this),
+      'result': this.showResult.bind(this),
+      'clear-input': this.clearInput.bind(this),
+      'invert-sign': this.invertSign.bind(this),
+      'square': this.square.bind(this),
+      'reciprocal': this.reciprocal.bind(this),
+      'devide': this.printOperationSymbol.bind(this),
+      'multiply': this.printOperationSymbol.bind(this),
+      'subtraction': this.printOperationSymbol.bind(this),
+      'addition': this.printOperationSymbol.bind(this),
+    };
+
     if ( extraActions ) {
       for ( var action_name in extraActions ) {
         this.actions_names[action_name] = extraActions[action_name];
       }
     }
+
   }
 
   bindActions( actions_to_bind ) {
@@ -54,46 +86,59 @@ class Calculator {
   	}.bind(this));
   }
 
-  addButton( button, container ){
-    var $button = $('<button></button');
-    container.append($button);
-    $button.text(button.label);
-    $button.css({
-      'background': button.background,
-      'width': '60px',
-      'height': '60px'  
-    });
-    var button_options = {
-        button: $button,
-        action: button.action,
-        value: button.value,
-        key: button.key
-      }
-    this.buttons[button.label] = button_options
+
+
+
+
+  // >>> INTERFACE >>>
+  addButton( button, $container ){
+
+    var $button = $('<button></button')
+      .appendTo( $container )
+      .text(button.label)
+      .css({
+        'background': button.background,
+        'width': '60px',
+        'height': '60px'  
+      })
+      .click( function(e){
+        if( button.action ) {
+          if( this.actions_names[ button.action ] ) this.actions_names[ button.action ](e);
+        }else this.printSymbol(e);
+      }.bind(this))
+    ;
+
   }
 
-  createCalculatorInterface(config_data, calculator) {
-    calculator.append('<div class="screen"></div>');
+  createCalculatorInterface(config_data, $container) {
+    
+    this.$screen = $('<div class="screen"></div>').appendTo( $container );
+
     config_data.buttons.forEach(function(button) {
-      this.addButton(button, calculator);
+      this.addButton(button, $container);
     }.bind(this));
-  }
 
+  }
+  // <<< INTERFACE <<<
+
+
+
+  // >>> ACTIONS >>>
   printOperationSymbol() {
     var input_symbols = event.key || event.target.innerText;
     if ( input_symbols.match(/[a-z]/g) ) return;
-    this.screen.text(this.screen.text() + ' ' + input_symbols + ' ');
+    this.$screen.text(this.$screen.text() + ' ' + input_symbols + ' ');
   }
 
   printSymbol(){
-    this.screen = $('.screen');
+
     var input_symbols = event.key || event.target.innerText;
     if ( input_symbols.match(/[a-z]/g) ) return;
-    this.screen.text(this.screen.text() + input_symbols);
+    this.$screen.text(this.$screen.text() + input_symbols);
   }
 
   showResult(){
-    var result_array = this.screen.text().split(' ');
+    var result_array = this.$screen.text().split(' ');
     var result_string = '';
     for ( var i = 0; i < result_array.length; i += 3 ) {
       if (result_array[i+1] == '/') result_string += parseFloat(result_array[i]) / parseFloat(result_array [i+2]);
@@ -102,48 +147,52 @@ class Calculator {
       if (result_array[i+1] == '+') result_string += parseFloat(result_array[i]) + parseFloat(result_array [i+2]);
     }
     if (result_array.length == 1) result_string = result_array[0];
-    this.screen.text( result_string.toString() );
+    this.$screen.text( result_string.toString() );
 
   }
 
   clearInput(){
-    this.screen.text('');
+    this.$screen.text('');
   }
 
   invertSign(){
     this.showResult();
-    this.screen.text((parseFloat(this.screen.text()) * (-1)).toString());
+    this.$screen.text((parseFloat(this.$screen.text()) * (-1)).toString());
   }
 
   square(){
     this.showResult();
-    this.screen.text((Math.sqrt( this.screen.text() )).toString());
+    this.$screen.text((Math.sqrt( this.$screen.text() )).toString());
   }
 
   reciprocal(){
   	this.showResult();
-    this.screen.text((1 / parseFloat(this.screen.text())).toString());
+    this.$screen.text((1 / parseFloat(this.$screen.text())).toString());
   }
 
 
   removeLastSymbol(){
-  	var all_input_array = this.screen.text().trim().split('');
+  	var all_input_array = this.$screen.text().trim().split('');
 		all_input_array.pop();
-    this.screen.text(all_input_array.join('').trim());
+    this.$screen.text(all_input_array.join('').trim());
   }
 
   setFloatPoint(){
-    this.screen.text(this.screen.text() + '.');
+    this.$screen.text(this.$screen.text() + '.');
   }
 
-  initMouseInput( calculator ) {
-    for (var button in this.buttons) {
-      if ( this.buttons[button].action ) this.buttons[button].button.on('click', this.actions_names[this.buttons[button].action]);
-      else this.buttons[button].button.on('click', this.printSymbol.bind(this));
-    }
-  }
+  // <<< ACTIONS <<<
 
-  initKeyboardInput( calculator ) {
+
+
+
+
+
+
+
+  // >>> INPUT >>>
+
+  initKeyboardInput( $container ) {
   	if( !this.onKeyPress ){
 		  this.onKeyPress = function(event){
 		  	var key = event.key;
@@ -154,7 +203,9 @@ class Calculator {
         }
 		  }.bind(this);
 		}
-  	calculator.on('keydown', this.onKeyPress);
+  	$container.on('keydown', this.onKeyPress);
   }
+  // <<< INPUT <<<
+
 }
     
--- calculator-test.js
+++ calculator-test.js
@@ -1,15 +1,1 @@
-$(function() {
-
-	var $element = $(".calculator");
-	if(!$element.length) return;
-
-	var config;
-	$.getJSON("config.json", function(data) {
-		config = data;
-		
-		var extraActions = {'current-year': function (){ return new Date().getFullYear();}}
-
-		let calculator = new Calculator( config, $element, extraActions );
-	});
-
-})
+//
